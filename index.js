692ea077a926ee662b77f27453dcee079630bb6a
Gloria van Weerden <gloria.vanweerden@gmail.com>
2021-04-27 22:25:55 -0400

    Commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = class extends Page {
     render(sPage) {
         const oJson = fetch("https://ux-308-e4737-default-rtdb.firebaseio.com/meals.json").json();
         console.log(oJson);
-        let sResult = "<h1>Mother's Day Breakfast and Brunch Popup Meal</h1>";
+        let sResult = "<h1>Mother's Day Breakfast and Brunch Popup Meal at Jay's Dinner</h1>";
         Object.keys(oJson).map((key) => {
             const oEntity = oJson[key];
             console.log(oEntity);
